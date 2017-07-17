4b363908ca852e413e8871e1cfe1ddf8ea72a5c6
NezSpencer <nuhiara@gmail.com>
2017-07-17 21:16:06 +0100

    init

---
+++ app/src/main/java/com/ANC_TeamEagles/mypurse/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ANC_TeamEagles.mypurse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.allItems) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
